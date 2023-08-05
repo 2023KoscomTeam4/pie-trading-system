b9a5f7b51aafc4b39ca5ab67bdffa04dd69839f8
2haein <eehaein2@gmail.com>
2023-08-05 22:33:52 +0900

    server 개발 환경 세팅

---
+++ Project/Pie-Server/src/main/java/com/hackerton/pieSystem/PieSystemApplication.java
@@ -0,0 +1,13 @@
+package com.hackerton.pieSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PieSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PieSystemApplication.class, args);
+	}
+
+}
