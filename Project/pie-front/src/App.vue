ee3c687449a1fed1ecabd03be262d80d4ffd4bd6
2haein <eehaein2@gmail.com>
2023-08-06 01:08:57 +0900

    vue-router, vuetify 재설정

---
+++ Project/pie-front/src/App.vue
@@ -0,0 +1,48 @@
+<!-- <template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style> -->
+
+<template>
+  <v-app>
+    <Navigation />
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import Navigation from '@/components/Navigation'
+export default {
+  name: "App",
+
+  components: {
+    Navigation,
+  },
+
+  data: () => ({  })};
+
+</script>
---
+++ Project/pie-front/src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="About">
+      <h1>This is an About Test</h1>
+    </div>
+  </template>
+  
